2571155c866a1154c5c2f5c7e4596b00286e8b75
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 16:37:25 +0530

    adding delay as constant

--- tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
+++ tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
@@ -20,6 +20,7 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#212121")
+val delay : Long = 20
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
@@ -105,7 +106,7 @@ class TriStrokeFillerView(ctx : Context) : View(ctx) {
             if (animated) {
                 cb()
                 try {
-                    Thread.sleep(50)
+                    Thread.sleep(delay)
                     view.invalidate()
                 } catch(ex : Exception) {
 
