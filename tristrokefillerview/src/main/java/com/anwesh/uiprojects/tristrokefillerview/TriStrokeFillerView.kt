002cd7a1c34e4b07de017f0ffa83100d9fee35ab
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 16:16:03 +0530

    adding TSFNode class

--- tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
+++ tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
@@ -125,4 +125,48 @@ class TriStrokeFillerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TSFNode(var i : Int, val state : State = State()) {
+
+        private var next : TSFNode? = null
+        private var prev : TSFNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = TSFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTSFNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TSFNode {
+            var curr : TSFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
