c6f659a54d264b4f84ee1fe61b902e12a67bce6d
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 16:23:38 +0530

    using static create method

--- tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
+++ tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
@@ -216,4 +216,13 @@ class TriStrokeFillerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : TriStrokeFillerView {
+            val view : TriStrokeFillerView = TriStrokeFillerView(activity)
+            activity.setContentView(view)
+            return view 
+        }
+    }
 }
