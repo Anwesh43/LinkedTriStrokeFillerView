dbae49e656a3602e8a0da1c6e8600d3366900c58
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 15:54:51 +0530

    adding connstants and importing libraries

--- tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
+++ tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.tristrokefillerview
 /**
  * Created by anweshmishra on 23/03/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 4
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#212121")
