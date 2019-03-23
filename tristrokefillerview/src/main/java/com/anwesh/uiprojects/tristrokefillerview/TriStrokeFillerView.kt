dca97dfaf2c19524ba2f3bd932fe8fb501292f01
Anwesh43 <anweshthecool0@gmail.com>
2019-03-23 16:18:09 +0530

    adding TriStrokeFiller class

--- tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
+++ tristrokefillerview/src/main/java/com/anwesh/uiprojects/tristrokefillerview/TriStrokeFillerView.kt
@@ -166,7 +166,31 @@ class TriStrokeFillerView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class TriStrokeFiller(var i : Int) {
+
+        private val root : TSFNode = TSFNode(0)
+        private var curr : TSFNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
